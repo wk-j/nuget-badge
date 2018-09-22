5bee61bdc1092653960fdcd5a18168d0f115a1b8
wk-j <somnuk.wk@gmail.com>
2018-09-22 14:49:47 +0700

    Initial commit

---
+++ src/NuGetBadge/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let template =
+        "[![NuGet](https://img.shields.io/nuget/v/{id}.svg)](https://www.nuget.org/packages/{id})"
+            .Replace("{id}", argv.[0])
+    template |> printfn "%s"
+    0
