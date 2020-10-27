146ef91d9cf564d56ccfbc1384158e736577c55a
wk <somnuk.wk@gmail.com>
2020-10-27 18:58:05 +0700

    Download

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 ## NuGet Badge
 
 [![NuGet](https://img.shields.io/nuget/v/wk.NuGetBadge.svg)](https://www.nuget.org/packages/wk.NuGetBadge)
+[![NuGet Downloads](https://img.shields.io/nuget/dt/wk.NuGetBadge.svg)](https://www.nuget.org/packages/wk.NuGetBadge)
 
 ## Installation
 
-```
+```bash
 dotnet tool install -g wk.NuGetBadge
 ```
 
 ## Usage
 
-```
+```bash
 wk-nuget-badge wk.NugetBadge
 ```
--- src/NuGetBadge/Program.fs
+++ src/NuGetBadge/Program.fs
@@ -4,8 +4,15 @@ open System
 
 [<EntryPoint>]
 let main argv =
+    let id = argv.[0]
+    let download =
+        "[![NuGet Downloads](https://img.shields.io/nuget/dt/{id}.svg)](https://www.nuget.org/packages/{id})"
+            .Replace("{id}", id)
     let template =
         "[![NuGet](https://img.shields.io/nuget/v/{id}.svg)](https://www.nuget.org/packages/{id})"
-            .Replace("{id}", argv.[0])
+            .Replace("{id}", id)
+
     template |> printfn "%s"
+    download |> printfn "%s"
+
     0
